2784c7a736cf06f199ab60ebd229ccaf15c29eac
Bruno Luiz Silva <contact@brunoluiz.net>
2021-03-03 09:47:06 +0000

    remove un-used enum

--- src/client/page/index.ts
+++ src/client/page/index.ts
@@ -6,19 +6,19 @@ import Url from "./url";
 import { snapshot } from "rrweb-snapshot";
 import { EmitterAccess } from "../access";
 
-export enum ProxyMessages {
-  PageDOMChanged = 1,
-  PageDOMRequested,
-  PageCursorMoved,
-  PageCursorClicked,
-  PageScrollChanged,
-  PageTextInputChanged,
-  PageUrlChanged,
-  PagePermissionsChanged,
+export enum PageMessages {
+  DOMChanged = 1,
+  DOMRequested,
+  CursorMoved,
+  CursorClicked,
+  ScrollChanged,
+  TextInputChanged,
+  URLChanged,
+  PermissionsChanged,
 }
 
 export interface Proxy {
-  send(type: ProxyMessages, payload: any): void;
+  send(type: PageMessages, payload: any): void;
   onMessage(cb: any): void;
   close(): void;
 }
@@ -45,7 +45,7 @@ export class Page {
   dump() {
     return (
       this.permissions.includes(EmitterAccess.DOMChange) &&
-      this.emitter.send(ProxyMessages.PageDOMChanged, {
+      this.emitter.send(PageMessages.DOMChanged, {
         html: snapshot(document, {
           blockClass: "remoteSecured",
           maskAllInputs: false,
@@ -61,27 +61,27 @@ export class Page {
   listen() {
     this.mouse.onMove((e) => {
       this.permissions.includes(EmitterAccess.CursorChange) &&
-        this.emitter.send(ProxyMessages.PageCursorMoved, e);
+        this.emitter.send(PageMessages.CursorMoved, e);
     });
 
     this.mouse.onClick((e) => {
       this.permissions.includes(EmitterAccess.CursorClick) &&
-        this.emitter.send(ProxyMessages.PageCursorClicked, e);
+        this.emitter.send(PageMessages.CursorClicked, e);
     });
 
     this.scroll.onChange((e) => {
       this.permissions.includes(EmitterAccess.ScrollChange) &&
-        this.emitter.send(ProxyMessages.PageScrollChanged, e);
+        this.emitter.send(PageMessages.ScrollChanged, e);
     });
 
     this.dom.onChange((e) => {
       this.permissions.includes(EmitterAccess.DOMChange) &&
-        this.emitter.send(ProxyMessages.PageDOMChanged, e);
+        this.emitter.send(PageMessages.DOMChanged, e);
     });
 
     this.textInput.onChange((e) => {
       this.permissions.includes(EmitterAccess.TextInputChange) &&
-        this.emitter.send(ProxyMessages.PageTextInputChanged, e);
+        this.emitter.send(PageMessages.TextInputChanged, e);
     });
   }
 
@@ -90,21 +90,21 @@ export class Page {
     if (upstream) return; // FIXME: there must be a better way than this
 
     switch (type) {
-      case ProxyMessages.PageScrollChanged:
+      case PageMessages.ScrollChanged:
         return this.scroll.update(payload);
-      case ProxyMessages.PageCursorMoved:
+      case PageMessages.CursorMoved:
         return this.mouse.update(payload);
-      case ProxyMessages.PageCursorClicked:
+      case PageMessages.CursorClicked:
         return this.mouse.click(payload);
-      case ProxyMessages.PageTextInputChanged:
+      case PageMessages.TextInputChanged:
         return this.textInput.update(payload);
-      case ProxyMessages.PageUrlChanged:
+      case PageMessages.URLChanged:
         return this.url.refresh(payload);
-      case ProxyMessages.PageDOMChanged:
+      case PageMessages.DOMChanged:
         return this.dom.update({
           ...payload,
         });
-      case ProxyMessages.PageDOMRequested:
+      case PageMessages.DOMRequested:
         return this.dump();
       default:
         return;
